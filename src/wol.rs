3475dae64a28e2d394e75cb5924863f34ea29e09
Mathias Pius <contact@pius.io>
2022-01-15 22:21:29 +0100

    Undo bad simplifications for WoL, and add a test.

--- src/wol.rs
+++ src/wol.rs
@@ -7,11 +7,11 @@ use crate::{Error, SyncRobot};
 #[derive(Debug, Deserialize)]
 pub struct WakeOnLan {
     #[serde(rename = "server_ip")]
-    pub ip: Ipv4Addr,
+    pub server_ipv4: Ipv4Addr,
     #[serde(rename = "server_ipv6_net")]
-    pub ipv6_net: Ipv6Addr,
+    pub server_ipv6_net: Ipv6Addr,
     #[serde(rename = "server_number")]
-    pub id: u32,
+    pub server_id: u32,
 }
 
 #[derive(Debug, Deserialize)]
@@ -42,5 +42,22 @@ where
     fn trigger_wol(&self, server_number: u32) -> Result<WakeOnLan, Error> {
         self.post::<WakeOnLanResponse, ()>(&format!("/wol/{}", server_number), ())
             .map(WakeOnLan::from)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::WakeOnLanRobot;
+    use crate::{Robot, ServerRobot};
+
+    #[test]
+    #[ignore]
+    fn get_wol() {
+        let robot = Robot::default();
+
+        let servers = robot.list_servers().unwrap();
+
+        let wol = robot.get_wol(servers[0].id).unwrap();
+        assert_eq!(wol.server_id, servers[0].id);
     }
 }
