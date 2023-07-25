3b3d2f3f881599aace117fdfbe71d66bafd8044e
Mathias Pius <contact@pius.io>
2023-07-25 22:55:39 +0200

    Product search listing

--- src/api/ordering/mod.rs
+++ src/api/ordering/mod.rs
@@ -1,3 +1,69 @@
 mod models;
+use std::ops::RangeInclusive;
 
 pub use models::*;
+use rust_decimal::Decimal;
+use serde::Serialize;
+
+use crate::{error::Error, AsyncRobot};
+
+use super::{wrapper::List, UnauthenticatedRequest};
+
+fn list_products(
+    monthly_price: Option<RangeInclusive<Decimal>>,
+    setup_price: Option<RangeInclusive<Decimal>>,
+    location: Option<&str>,
+) -> Result<UnauthenticatedRequest<List<Product>>, serde_html_form::ser::Error> {
+    #[derive(Debug, Serialize)]
+    struct ProductSearch<'a> {
+        min_price: Option<Decimal>,
+        max_price: Option<Decimal>,
+        min_price_setup: Option<Decimal>,
+        max_price_setup: Option<Decimal>,
+        location: Option<&'a str>,
+    }
+
+    UnauthenticatedRequest::from("https://robot-ws.your-server.de/order/server/product").with_body(
+        ProductSearch {
+            min_price: monthly_price.as_ref().map(|p| *p.start()),
+            max_price: monthly_price.as_ref().map(|p| *p.end()),
+            min_price_setup: setup_price.as_ref().map(|p| *p.start()),
+            max_price_setup: setup_price.as_ref().map(|p| *p.end()),
+            location,
+        },
+    )
+}
+
+impl AsyncRobot {
+    pub async fn list_products(
+        &self,
+        monthly_price: Option<RangeInclusive<Decimal>>,
+        setup_price: Option<RangeInclusive<Decimal>>,
+        location: Option<&str>,
+    ) -> Result<Vec<Product>, Error> {
+        Ok(self
+            .go(list_products(monthly_price, setup_price, location)?)
+            .await?
+            .0)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use tracing::info;
+    use tracing_test::traced_test;
+
+    use crate::AsyncRobot;
+
+    #[tokio::test]
+    #[traced_test]
+    async fn get_product_listing() {
+        dotenvy::dotenv().ok();
+
+        let robot = AsyncRobot::default();
+
+        for product in robot.list_products(None, None, None).await.unwrap() {
+            info!("{product:#?}");
+        }
+    }
+}
--- src/api/ordering/models.rs
+++ src/api/ordering/models.rs
@@ -1,1 +1,45 @@
+use bytesize::ByteSize;
+use rust_decimal::Decimal;
+use serde::Deserialize;
 
+#[derive(Debug, Clone, Deserialize)]
+pub struct Product {
+    pub id: String,
+    pub name: String,
+    pub description: Vec<String>,
+
+    /// Monthly traffic limitation if any, e.g. `5 TB`.
+    #[serde(deserialize_with = "crate::conversion::traffic")]
+    pub traffic: Option<ByteSize>,
+
+    pub dist: Vec<String>,
+    pub lang: Vec<String>,
+    pub location: Vec<String>,
+    pub prices: Vec<LocationPrice>,
+    pub orderable_addons: Vec<Addon>,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct LocationPrice {
+    pub location: String,
+    #[serde(rename = "price")]
+    pub monthly: Price,
+    #[serde(rename = "price_setup")]
+    pub setup: Price,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct Price {
+    pub net: Decimal,
+    pub gross: Decimal,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct Addon {
+    pub id: String,
+    pub name: String,
+    pub location: Option<String>,
+    pub min: u32,
+    pub max: u32,
+    pub prices: Vec<LocationPrice>,
+}
